27870d759454a92b01393f4b1300f81f1dc5c962
Kokimoto <suportekokimoto@gmail.com>
2026-07-26 17:26:36 -0300

    Add debug to api/index.php to show real error

--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 define('LARAVEL_START', microtime(true));
 
@@ -13,14 +13,21 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
     require $maintenance;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+try {
+    require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/vercel.php';
+    $app = require_once __DIR__ . '/../bootstrap/vercel.php';
 
-$kernel = $app->make(Kernel::class);
+    $kernel = $app->make(Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
 
-$kernel->terminate($request, $response);
+    $kernel->terminate($request, $response);
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "Error: " . $e->getMessage() . "\n";
+    echo "File: " . $e->getFile() . ":" . $e->getLine() . "\n";
+    echo "Trace: " . $e->getTraceAsString();
+}
