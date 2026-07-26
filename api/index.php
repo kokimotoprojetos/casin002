0efc57c752a8db5da26385ee61b6b4c5cc67cff9
Kokimoto <suportekokimoto@gmail.com>
2026-07-26 16:46:27 -0300

    Fix api/index.php - use Kernel handle instead of handleRequest

--- api/index.php
+++ api/index.php
@@ -1,17 +1,22 @@
 <?php
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-$application = require_once __DIR__.'/../bootstrap/app.php';
-$application->handleRequest(Request::capture());
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
