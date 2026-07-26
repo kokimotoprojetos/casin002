e60826f41f9c39d972429af009d2e02142663d91
Kokimoto <suportekokimoto@gmail.com>
2026-07-26 17:20:28 -0300

    feat: habilita exibicao de erros na vercel e cache de bootstrap em tmp

--- bootstrap/vercel.php
+++ bootstrap/vercel.php
@@ -31,6 +31,19 @@ if ($isVercel) {
     $_ENV['MODULES_STATUSES_PATH'] = $modulesFile;
     $_SERVER['MODULES_STATUSES_PATH'] = $modulesFile;
     putenv("MODULES_STATUSES_PATH={$modulesFile}");
+
+    $bootstrapPath = '/tmp/bootstrap';
+    if (!is_dir($bootstrapPath . '/cache')) {
+        mkdir($bootstrapPath . '/cache', 0755, true);
+        if (is_dir(__DIR__ . '/cache')) {
+            foreach (glob(__DIR__ . '/cache/*') as $file) {
+                if (is_file($file)) {
+                    @copy($file, $bootstrapPath . '/cache/' . basename($file));
+                }
+            }
+        }
+    }
+    $app->useBootstrapPath($bootstrapPath);
 }
 
 $app->useStoragePath($storagePath);
